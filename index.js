4ede0c85af6d3e5485042f46032c87de6b7322d0
Pedifax <kartd02606@gmail.com>
2024-04-10 23:44:25 -0500

    Updated MongoDB connection string to use the new format

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ error_handler(app);
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster.qozaawu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.${process.env.DB_URL}=${process.env.DB_NAME}`
   )
   .then(() => {
     const port = process.env.PORT || 4000;
